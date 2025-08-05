7d4129ac0e481e637f5e959ad29965aa5411a1b1
sahil-salunke <sahil.salunke6194@gmail.com>
2025-08-05 22:22:58 +0530

    Test case added for Util

---
+++ src/test/java/KafkaUtilTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KafkaUtilTest {
+
+    @Test
+    void testConstants() {
+        assertEquals("email-notification", KafkaUtil.KAFKA_TOPIC);
+        assertEquals("email-consumer-group", KafkaUtil.KAFKA_GROUP_ID);
+    }
+
+    @Test
+    void testPrivateConstructor() throws Exception {
+        Constructor<KafkaUtil> constructor = KafkaUtil.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        InvocationTargetException thrown = assertThrows(InvocationTargetException.class, constructor::newInstance);
+        assertNotNull(thrown.getCause());
+    }
+}
