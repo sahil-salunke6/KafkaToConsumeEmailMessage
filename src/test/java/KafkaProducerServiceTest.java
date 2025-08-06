3c6d912450ecdfc67aef25cd8fb275ee130d4d8d
sahil-salunke <sahil.salunke6194@gmail.com>
2025-08-06 20:19:55 +0530

    Testcase updated

--- src/test/java/KafkaProducerServiceTest.java
+++ src/test/java/KafkaProducerServiceTest.java
@@ -3,21 +3,36 @@ import org.example.service.KafkaProducerService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
-import static org.mockito.Mockito.*;
+import java.util.concurrent.CompletableFuture;
 
-public class KafkaProducerServiceTest {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class KafkaProducerServiceTest {
 
     @Test
-    void testSendEmail() {
+    void testSendEmailSuccessAndFailure() {
         KafkaTemplate<String, EmailNotification> kafkaTemplate = Mockito.mock(KafkaTemplate.class);
         KafkaProducerService producerService = new KafkaProducerService(kafkaTemplate);
 
         EmailNotification notification = new EmailNotification();
-        notification.setSubject("Test Subject");
 
+        // Success future
+        CompletableFuture<SendResult<String, EmailNotification>> successFuture = CompletableFuture.completedFuture(Mockito.mock(SendResult.class));
+        when(kafkaTemplate.send(eq("email-notification"), any(EmailNotification.class)))
+                .thenReturn(successFuture)
+                .thenReturn(CompletableFuture.failedFuture(new RuntimeException("Kafka send failed")));
+
+        // Call once for success
+        producerService.sendEmail(notification);
+        // Call again for failure
         producerService.sendEmail(notification);
 
-        verify(kafkaTemplate, times(1)).send("email-notification", notification);
+        verify(kafkaTemplate, times(2)).send("email-notification", notification);
     }
 }
